e8c7f19ecd5c10a10e72c90b1fc73ca520ce62f3
GMShimon <simon.rosedale99@gmail.com>
2021-10-18 23:43:57 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5uQbctlUSP8an3u1VJhIpn8qL1ZUFuj0",
+    authDomain: "fitness-healthcare.firebaseapp.com",
+    projectId: "fitness-healthcare",
+    storageBucket: "fitness-healthcare.appspot.com",
+    messagingSenderId: "1549515147",
+    appId: "1:1549515147:web:dec83cc367e5c15a4d978e"
+};
+export default firebaseConfig;
